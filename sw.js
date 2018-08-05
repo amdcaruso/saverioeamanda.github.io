ca6ca5bcd722178cdb79f9fe6edcd47f37e84cc6
Amanda Cavallaro <amdcaruso@gmail.com>
2018-08-05 13:48:00 +0100

    feat: Add service worker

---
+++ sw.js
@@ -0,0 +1,39 @@
+var staticCacheName = 'wedding-static-v2';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        './css/styles.css',
+        './js/main.js',
+        './index.html',
+        './restaurant.html',
+        './pt.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wedding-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
